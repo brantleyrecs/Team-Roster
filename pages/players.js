8a90dd8db245635abd175f4be73748e8c43fc24f
Michael Brantley <brantleyrecs@gmail.com>
2023-07-29 09:34:34 -0500

    added search bar

--- pages/players.js
+++ pages/players.js
@@ -19,12 +19,21 @@ export default function Players() {
     getAllThePlayers();
   }, []);
 
+  const filterResult = (query) => {
+    if (!query) {
+      getAllThePlayers();
+    } else {
+      const filter = players.filter((player) => player.first_name.toLowerCase().includes(query) || player.last_name.toLowerCase().includes(query));
+      setPlayers(filter);
+    }
+  };
+
   return (
     <div className="text-center my-4">
       <Link href="/player/new" passHref>
         <Button style={{ marginBottom: '20px' }}>Add A Player</Button>
       </Link>
-      <SearchBar />
+      <SearchBar onKeyUp={(query) => filterResult(query)} />
       <div className="d-flex flex-wrap">
         {players.map((player) => (
           <PlayerCard key={player.firebaseKey} playerObj={player} onUpdate={getAllThePlayers} />
